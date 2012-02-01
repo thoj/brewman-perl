acaeb637d7f4840d8e76b347012e76c2823d2a31
Thomas Jager <mail@jager.no>
2012-02-01 21:28:28 +0100

    controller

--- controller.pl
+++ controller.pl
@@ -1,43 +1,77 @@
 #/usr/bin/perl
+use strict;
+use warnings;
 
-my $Kp = 0.1;
-my $Kd = 4;
-my $Ki = 0.2;
 
-my $output = 0;
+my $Kp = 0.4;
+my $Kd = 0.01;
+my $Ki = 0.1;
 
+my $output = 50;
 my $output_max = 100;
 my $output_min = 0;
 
-my $last_pv = 51;
+my $deadband = 5;
+my $last_output = 50;
 
-my $pv = 50;
-my $sp = 55;
 
+my $pv_last = read_pv();
+my $sp = read_sp();
+
+my $pv = 0;
 my $iterm = 0;
 
-while (sleep 2) {
-	#Read PV and SP
-	print "PV $pv ";
-	
+# period is 60 seconds.
+
+while (sleep 6) {
+	$pv = read_pv();	
+
 	# Calculate integral.
 	my $error = $sp - $pv;
 	$iterm += ($Ki * $error);
-#	print "Iterm: $iterm\n";
+	# Limit integral
 	if ($iterm > $output_max) {$output = $output_max; }
 	if ($iterm < $output_min) {$output = $output_min; }
 	
+	# Calculate derivative	
 	my $dterm = ($pv - $pv_last);
 
-	$output = ($Kp * $error) + $iterm - $Kd * $dterm;
-	print "$output ";
+	# Calculate output
+	$output = $Kp * $error + $iterm - $Kd * $dterm;
 	
+	# Limit output
 	if ($output > $output_max) {$output = $output_max; }
 	if ($output < $output_min) {$output = $output_min; }
 	
-	print " Output: $output\n";
+	print "$output E: $error I: $iterm D: $dterm PV: $pv PV_Last: $pv_last\n";
 	
-	$pv = 100 * ($output / 100);
 	$pv_last = $pv;
 	
+	# Here i need to deside if i need to inc my 3 pole controller 
+	# or dec. Not sure of the best way to do this yet.
+	
+	if ($output > 50 && $output - 50 > $deadband) 
+	{
+		#Send increase pulse
+		print "Inc.\n"
+	} elsif ($output < 50 && $output - 50 < -$deadband) {
+		#Send decrease pulse
+		print "Dec.\n";
+	} else {
+		print "Nop.\n";
+	}
+	
+}
+
+sub read_sp {
+	# Read SP from file?
+	# Fake it for now
+	return 55;
+}
+
+sub read_pv
+{
+	# Read PV from sensors
+	# Fake it for now
+	return $output*0.87;
 }
